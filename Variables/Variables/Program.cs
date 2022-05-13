d50bcd16e0d588cd709f713289b149b4d1b648d5
ssanthu81 <101133033+ssanthu81@users.noreply.github.com>
2022-05-13 09:11:46 -0500

    Add files via upload

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,17 @@
+﻿class First
+{
+    int i = 10;
+    static void Main(string[] args) {
+
+        First f = new First();
+        First f1 = new First();
+        First f2 = new First();
+        Console.WriteLine(f.i + "   " + f1.i);
+        f2.i = 30;
+        Console.WriteLine(f.i + "   " + f2.i);
+      
+        Console.ReadLine();
+
+    }
+
+}
